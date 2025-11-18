1a3e6218c1a85af0cd0f7922657894421a5a0e0d
Mikel <mikelnaval2006@gmail.com>
2025-11-18 17:00:25 +0100

    Ejercicio BD DWES U3

--- BBDDUD03-DWES/src/GestorLectura.php
+++ BBDDUD03-DWES/src/GestorLectura.php
@@ -1,9 +1,46 @@
 <?php
 namespace Mikel;
+require_once '../tools/Conexion.php';
 require "../vendor/autoload.php";
 
 class GestorLectura implements AccionesBD {
 
+
+    // Atributos
+    private $conexion;
+    function actualizar($id, array $datos){
+        foreach($datos as $campo) {
+            $titulo = $datos['titulo_libro'];
+            $autor = $datos['autor'];
+            $terminado = $datos['terminado'];
+            $paginas = $datos['paginas'];
+            $fecha = $datos['fecha_lectura'];
+        // update datos
+        $updt = "UPDATE lectura SET id = $id, titulo_lectura = $titulo, autor = $autor, paginas = $paginas, terminados = $terminado, fecha_lectura = $fecha
+                WHERE id = $id";
+            
+        }
+    }
+    function listar(){
+        
+        
+        $select = "SELECT id,titulo_lectura,autor,paginas,fecha_lectura FROM lectura";
+        $arrayLibros = $conexion->query($select);
+        
+        return $arrayLibros;
+    }
+    function insertar(array $datos) {
+        
+        foreach($datos as $l){
+            $titulo = $l['titulo_libro'];
+            $autor = $l['autor'];
+            $paginas = $l['paginas'];
+            $fecha_lectura = $l['fecha_lectura'];
+            $i = "INSERT INTO lectura(titulo_lectura,autor,paginas,fecha_lectura)
+            VALUES ($titulo,$autor,$paginas,$fecha_lectura);";
+        }
+    }
+    
     
 }
 
--- BBDDUD03-DWES/tools/Conexion.php
+++ BBDDUD03-DWES/tools/Conexion.php
@@ -16,11 +16,4 @@ class Conexion {
     $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
 }
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
- ?>
+?>
