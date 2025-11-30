53450fdb8866fb88d30e48a3e64b10f24f75506f
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2025-11-30 14:01:49 +0100

    Modified

---
+++ EXAMENUD1/script.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+
+// VARIABLES
+const nums = prompt("Introduce números separados cada uno (,): ");
+function mostrar(nums){}
+
+
+
+function countGoodPairs(nums){}
