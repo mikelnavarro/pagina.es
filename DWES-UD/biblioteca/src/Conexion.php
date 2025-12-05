32650a7690b17aad5cbc9b790fda896c6b537142
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2025-12-05 12:40:00 +0100

    Ejercicio Biblioteca (Proyecto DOCX)

---
+++ DWES-UD/biblioteca/src/Conexion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mikel\Biblioteca;
+
+class Conexion
+{
+    private $servername = "localhost";
+    private $username = "dweb";
+    private $password = "12345";
+    private $bd = "biblioteca"
+    public $conexion;
+
+    public function __construct()
+    {
+        try {
+            $this->conexion = new PDO("mysql:host=$this->servername;dbname=$this->bd", $this->username, $this->password);
+            $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo $exception->getMessage();
+        }
+    }
+}
+
+?>
